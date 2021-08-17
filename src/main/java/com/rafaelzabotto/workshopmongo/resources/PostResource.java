6962d151cf210b2d67d30b0fb1f2a2861e72e890
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2021-08-16 22:35:05 -0300

    Using query methods

--- src/main/java/com/rafaelzabotto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/rafaelzabotto/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.rafaelzabotto.workshopmongo.resources;
 
 import com.rafaelzabotto.workshopmongo.domain.Post;
+import com.rafaelzabotto.workshopmongo.resources.util.URL;
 import com.rafaelzabotto.workshopmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -18,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method= RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text",defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
